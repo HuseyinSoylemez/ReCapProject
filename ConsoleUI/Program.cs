c86371ba3a03f71bad8b6de287f03619cdae5310
HuseyinSoylemez <huseyinsoylemez02@yandex.com>
2021-04-18 15:19:07 +0300

    8. gün

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,13 +12,65 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
+            //CarManager carManager = new CarManager(new InMemoryCarDal());
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine(car.Description);
+            //}
+
+            CarTest();
+
+            //ColorTest();
+        }
+
+        private static void CarTest()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
             foreach (var car in carManager.GetAll())
             {
-                Console.WriteLine(car.Description);
+                Console.WriteLine("Araba Adı: {0} - Özellik: {1}",car.CarName,car.Description);
+            }
+            Console.WriteLine("--------------------------------------------------");
+            foreach (var car in carManager.GetCarsByBrandId(2))
+            {
+                Console.WriteLine("Araba Adı: {0} - Özellik: {1}", car.CarName, car.Description);
+            }
+            Console.WriteLine("--------------------------------------------------");
+            foreach (var car in carManager.GetByDailyPrice(500, 800))
+            {
+                Console.WriteLine("Araba Adı: {0} - Özellik: {1}", car.CarName, car.Description);
+            }
+            Console.WriteLine("--------------------------------------------------");
+            foreach (var car in carManager.GetCarsByColorId(3))
+            {
+                Console.WriteLine("Araba Adı: {0} - Özellik: {1}", car.CarName, car.Description);
+            }
+        }
+
+        private static void ColorTest()
+        {
+            Console.WriteLine("-----------------------R E N K---------------------------");
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+            foreach (var color in colorManager.GetAll())
+            {
+                Console.WriteLine(color.ColorName);
+            }
+            Color color1 = new Color();
+            color1.ColorName = "Gri";
+            colorManager.Add(color1);
+
+            Console.WriteLine("-----------------------R E N K EKLENDİ---------------------------");
+            foreach (var color in colorManager.GetAll())
+            {
+                Console.WriteLine(color.ColorName);
             }
 
-
+            Console.WriteLine("-----------------------R E N K Silindi---------------------------");
+            colorManager.Delete(color1);
+            foreach (var color in colorManager.GetAll())
+            {
+                Console.WriteLine(color.ColorName);
+            }
         }
     }
 }
--- DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
@@ -14,11 +15,11 @@ namespace DataAccess.Concrete.InMemory
         {
             _cars = new List<Car>
             {
-                 new Car {Id = 1, BrandId = 1, ColorId = 1, ModelYear = 2018, DailyPrice = 500, Description = "Benzin"},
-                new Car {Id = 2, BrandId = 1, ColorId = 3, ModelYear = 2021, DailyPrice = 450, Description = "Benzin"},
-                new Car {Id = 3, BrandId = 2, ColorId = 2, ModelYear = 2020, DailyPrice = 800, Description = "Benzin+LPG"},
-                new Car {Id = 4, BrandId = 3, ColorId = 4, ModelYear = 2015, DailyPrice = 600, Description = "Dizel"},
-                new Car {Id = 5, BrandId = 3, ColorId = 2, ModelYear = 2016, DailyPrice = 300, Description = "Dizel"},
+                 new Car {Id = 1, BrandId = 1, ColorId = 1,CarName="Polo", ModelYear = 2018, DailyPrice = 500, Description = "Benzin"},
+                new Car {Id = 2, BrandId = 1, ColorId = 3, CarName="Focus",ModelYear = 2021, DailyPrice = 450, Description = "Benzin"},
+                new Car {Id = 3, BrandId = 2, ColorId = 2,CarName="Polo", ModelYear = 2020, DailyPrice = 800, Description = "Benzin+LPG"},
+                new Car {Id = 4, BrandId = 3, ColorId = 4, CarName="Polo",ModelYear = 2015, DailyPrice = 600, Description = "Dizel"},
+                new Car {Id = 5, BrandId = 3, ColorId = 2, CarName="Polo",ModelYear = 2016, DailyPrice = 300, Description = "Dizel"},
             };
         }
         public void Add(Car car)
@@ -31,9 +32,19 @@ namespace DataAccess.Concrete.InMemory
            _cars.Remove(_cars.SingleOrDefault(c=>c.Id==car.Id));
         }
 
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Car> GetAll()
         {
             return _cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
         }
 
         public List<Car> GetById(int carId)
@@ -47,6 +58,8 @@ namespace DataAccess.Concrete.InMemory
 
             carToUpdate.BrandId = car.BrandId;
             carToUpdate.ColorId = car.ColorId;
+            carToUpdate.CarName = car.CarName;
+            
             carToUpdate.DailyPrice = car.DailyPrice;
             carToUpdate.Description = car.Description;
             carToUpdate.ModelYear = car.ModelYear;
